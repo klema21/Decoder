4bc109b756272a6f976fa68ee8363227a8f59c1f
Ilya Klementev <kl3mentevilya@yandex.ru>
2024-09-27 10:58:41 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Install and build:
+
+1. Linux install: sudo apt install libicu-dev
+2. Compile and run: clang++ -licuuc main.cpp
---
+++ main.cpp
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "unicode/utypes.h"
+#include "unicode/ustring.h"
+#include "unicode/regex.h"
+#include "unicode/ucnv.h"
+#include "unicode/uclean.h"
+
+using namespace icu;
+
+
+const char *fileName;
+int         fileLen;
+
+char16_t *ucharBuf = nullptr;
+
+char *charBuf = nullptr;
+
+void readFile(const char *name) {
+
+    fileName = name;
+    fileLen  = 0;
+
+
+    FILE *file = fopen(name, "rb");
+    if (file == nullptr) {
+        fprintf(stderr, "ugrep: Could not open file \"%s\"\n", fileName);
+        return;
+    }
+    fseek(file, 0, SEEK_END);
+    int rawFileLen = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    
+
+    charBuf = static_cast<char*>(realloc(charBuf, rawFileLen + 1));
+    int t = static_cast<int>(fread(charBuf, 1, rawFileLen, file));
+    if (t != rawFileLen)  {
+        fprintf(stderr, "Error reading file \"%s\"\n", fileName);
+        fclose(file);
+        return;
+    }
+    charBuf[rawFileLen]=0;
+    fclose(file);
+
+    int32_t        signatureLength;
+    const char *   charDataStart = charBuf;
+    UErrorCode     status        = U_ZERO_ERROR;
+    const char*    encoding      = ucnv_detectUnicodeSignature(
+                           charDataStart, rawFileLen, &signatureLength, &status);
+    if (U_FAILURE(status)) {
+        fprintf(stderr, "ugrep: ICU Error \"%s\" from ucnv_detectUnicodeSignature()\n",
+            u_errorName(status));
+        return;
+    }
+    if(encoding!=nullptr ){
+        charDataStart  += signatureLength;
+        rawFileLen     -= signatureLength;
+    }
+
+    UConverter* conv;
+    conv = ucnv_open(encoding, &status);
+    if (U_FAILURE(status)) {
+        fprintf(stderr, "ugrep: ICU Error \"%s\" from ucnv_open()\n", u_errorName(status));
+        return;
+    }
+
+    uint32_t destCap = ucnv_toUChars(conv,
+                       nullptr,
+                       0,
+                       charDataStart,
+                       rawFileLen,
+                       &status);
+    if (status != U_BUFFER_OVERFLOW_ERROR) {
+        fprintf(stderr, "ugrep: ucnv_toUChars: ICU Error \"%s\"\n", u_errorName(status));
+        return;
+    };
+    
+    status = U_ZERO_ERROR;
+    ucharBuf = static_cast<char16_t*>(realloc(ucharBuf, (destCap + 1) * sizeof(char16_t)));
+    ucnv_toUChars(conv,
+        ucharBuf,
+        destCap+1,
+        charDataStart,
+        rawFileLen,
+        &status);
+    if (U_FAILURE(status)) {
+        fprintf(stderr, "ugrep: ucnv_toUChars: ICU Error \"%s\"\n", u_errorName(status));
+        return;
+    };
+    ucnv_close(conv);
+    
+    fileLen = destCap;
+}
+
+auto main() -> int {
+  return -1;
+}
